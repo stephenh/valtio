a76179bd17cfe54b6c18896a2d29fe9564d849dd
Stephen Haberman <stephen.haberman@gmail.com>
2023-02-20 18:25:37 -0600

    Comments, minor.

--- src/react/utils/useStore.ts
+++ src/react/utils/useStore.ts
@@ -8,35 +8,37 @@ const GET_STORE_SYMBOL = Symbol()
 /**
  * Provides a component with usage-based tracking of changes to a store.
  *
- * Instead of using a snapshot, and compare-proxying against that, we access proxy
+ * Instead of using a snapshot, and proxy-comparing against that, we access proxy
  * against the store itself, which means to detect changes we can't use the
  * `isChanged` from proxy-compare, but instead track both accessed + mutations,
  * and use the intersection of those to determine if we've had a dirty read.
  *
  * Current pros/cons:
  *
- * - Pro: removes complexity of separate snap/store
+ * - Pro: removes the complexity/ergonomics of separate snap/store
  * - Pro: removes need to "JSX loops over snapshots, but pass the store to children"
  * - Pro: works in both parent component & child components that each of their
  *     own `useStore` calls
  * - Pro: access proxies are stable/unstable based on their change since
  *     last render, so should "just work" with deps arrays/React.memos
+ * - Con: Doesn't use snapshots, and so is stuck with `useSES` Level 2
+ *     semantics--enforced consistency, but potentially long renders.
  *
  * @example
  * function MyComponent() {
- *  const store = useStore(props.proxy)
- *  return (
- *    <>
- *      <div onClick={() => store.count++}>
- *        {store.count}
- *      </div>
- *    </>
+ *   const store = useStore(props.proxy)
+ *   return (
+ *     <>
+ *       <div onClick={() => store.count++}>
+ *         {store.count}
+ *       </div>
+ *     </>
  *  );
  */
 export function useStore<T extends object>(store: T): T {
   // If we were passed one of our own access proxies, i.e. a child component
   // using `useStore`, unwrap it to establish the child component's own tracking
-  store = getStore(store)
+  store = getStore(store) ?? store
 
   const ref = useRef<UseStoreAdmin | null>(null)
   if (!ref.current) {
@@ -148,7 +150,8 @@ class StoreStats {
         const [op, path] = tuple
         // Only listen to changes directly to this store
         if (path.length === 1) {
-          this.changes.add(String(path[0]))
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          this.changes.add(path[0]!)
           if (op === 'set' && tuple[4]) {
             this.keysChange = true
           } else if (op === 'delete') {
@@ -194,5 +197,6 @@ class StoreStats {
   }
 }
 
-const getStore = <T extends object>(obj: T) =>
-  (obj as { [GET_STORE_SYMBOL]?: typeof obj })[GET_STORE_SYMBOL] || obj
+function getStore<T extends object>(obj: T): T | undefined {
+  return (obj as { [GET_STORE_SYMBOL]?: typeof obj })[GET_STORE_SYMBOL]
+}
